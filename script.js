83a7dbd467e8ed57025ea0bb126f1a3abd700768
ProvisorioMyName <provisoriomyname@gmail.com>
2026-08-18 18:47:16 -0300

    Resultados de respostas

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const bSobre = document.querySelector("#bsobre");
 const sobreEstilo = document.querySelector(".estiloSobre");
 const menu = document.querySelector(".estiloMenu");
 
+//Botão de sobre
 bSobre.addEventListener("click", () => {
   sobreEstilo.classList.remove("desativado");
   menu.classList.add("desativado");
 });
 
+//Botão da seta de voltar
 const voltar = document.getElementById("voltar");
-
 voltar.addEventListener("click", () => {
   sobreEstilo.classList.add("desativado");
   menu.classList.remove("desativado");
   footer.classList.remove("desativado");
 });
 
+//Botão de começar o quiz
 const start = document.querySelector("#bcomecar");
 const quiz = document.querySelector("#Quiz");
 const footer = document.querySelector("#footer");
-
 start.addEventListener("click", () => {
   menu.classList.add("desativado");
   quiz.classList.remove("desativado");
@@ -26,48 +27,51 @@ start.addEventListener("click", () => {
   acertos = 0;
   quantidadePergunta = 0;
   questaoUsada = [];
+  respostas = [];
   numQuestao.innerHTML = "1";
 
   mostrarTextos();
 });
 
-// Perguntas
+//
+// PERGUNTAS
+//
 
 const enunciados = [
   {
     pergunta: "Quem construiu a arca de 'Moisés'?",
     opcoes: ["Davi", "Moisés", "Noé", "Salomão"],
-    resposta: "Noé",
+    respostaCerta: "Noé",
   },
   {
-    pergunta: "O que indica BPM em uma música",
+    pergunta: "O que indica BPM em uma música?",
     opcoes: [
       "Batimentos do pulmão por minuto",
-      "Batimentos por minutos",
+      "Batimentos por minuto",
       "O volume",
       "A quantidade de instrumentos",
     ],
-    resposta: "Batimentos por minutos",
+    respostaCerta: "Batimentos por minuto",
   },
   {
     pergunta: "Quem traiu Jesus?",
     opcoes: ["Judas Iscariotes", "Judas Tadeu", "Judas", "Quem é Judas?"],
-    resposta: "Judas Iscariotes",
+    respostaCerta: "Judas Iscariotes",
   },
   {
     pergunta: "Qual livro conta a história de uma rainha que salvou seu povo?",
     opcoes: ["Rute", "Ester", "Juízes", "Neemias"],
-    resposta: "Ester",
+    respostaCerta: "Ester",
   },
   {
-    pergunta: 'Qual música não faz parte do álbum "Meia Noite" (FHOP)?',
+    pergunta: "Qual dessas expressões ficou famosa em O Rei Leão?",
     opcoes: [
-      "Quebra o silêncio",
-      "Dono da minha afeição",
-      "Meia noite",
-      "Em memória de Cristo",
+      "Hakuna Matata",
+      "Hakuna Marata",
+      "Hakuna Makata",
+      "Hakuna Manata",
     ],
-    resposta: "Em memória de Cristo",
+    respostaCerta: "Hakuna Matata",
   },
   {
     pergunta:
@@ -78,7 +82,7 @@ const enunciados = [
       "São primos",
       "Não possuem relação familiar",
     ],
-    resposta: "São irmãos",
+    respostaCerta: "São irmãos",
   },
   {
     pergunta: "Qual é o nome do assassino da franquia Sexta-Feira 13?",
@@ -88,17 +92,17 @@ const enunciados = [
       "Leatherface",
       "Freddy Krueger",
     ],
-    resposta: "Jason Voorhees",
+    respostaCerta: "Jason Voorhees",
   },
   {
     pergunta: "Qual destas é uma nota musical?",
     opcoes: ["Violeta", "Sol", "Lua", "Azul"],
-    resposta: "Sol",
+    respostaCerta: "Sol",
   },
   {
     pergunta: "Qual apóstolo era conhecido anteriormente como Saulo?",
     opcoes: ["Pedro", "Paulo", "Barnabé", "Tomé"],
-    resposta: "Paulo",
+    respostaCerta: "Paulo",
   },
   {
     pergunta: "Qual a função mais comum do baixo em uma banda?",
@@ -108,18 +112,18 @@ const enunciados = [
       "Tocar notas agudas",
       "Brigar com o guitarrista",
     ],
-    resposta: "Sustentar a base harmônica e rítmica",
+    respostaCerta: "Sustentar a base harmônica e rítmica",
   },
   {
-    pergunta: "Em qual filme aparece a boneca Annabelle?",
-    opcoes: ["Annabelle", "O exorcista", "Invocação do Mal", "Pânico"],
-    resposta: "Invocação do Mal",
+    pergunta: "Em qual filme teve o primeiro aparecimento da boneca Annabelle?",
+    opcoes: ["Annabelle", "O Exorcista", "Invocação do Mal", "Pânico"],
+    respostaCerta: "Invocação do Mal",
   },
   {
     pergunta:
       "Em qual cidade se passa principalmente Miraculous: As Aventuras de Ladybug?",
     opcoes: ["Londres", "Paris", "Tóquio", "Nova York"],
-    resposta: "Paris",
+    respostaCerta: "Paris",
   },
   {
     pergunta: "Quais são os nomes das três Meninas Superpoderosas?",
@@ -129,22 +133,24 @@ const enunciados = [
       "Lindinha, Docinho e Margarida",
       "Ervinha da Ninha, Feinha e Azedinha",
     ],
-    resposta: "Florzinha, Lindinha e Docinho",
+    respostaCerta: "Florzinha, Lindinha e Docinho",
   },
   {
-    pergunta: "Complete: 'Salvar pessoas, caçar coisas. ________ família'",
-    opcoes: ["O negócio da", "A tradição da", "A missão da", "O legado da"],
-    resposta: "O negócio da",
+    pergunta: "Quantos livros tem a Bíblia protestante?",
+    opcoes: ["66", "67", "65", "68"],
+    respostaCerta: "66",
   },
   {
     pergunta:
       "Qual é o nome do animatrônico que possui aparência de uma raposa em FNAF?",
     opcoes: ["Bonnie", "Foxy", "Chica", "Freddy"],
-    resposta: "Foxy",
+    respostaCerta: "Foxy",
   },
 ];
 
+//
 // PARTE DO QUIZ
+//
 
 const botoes = document.querySelectorAll(".Pbutton");
 const fim = document.querySelector("#fim");
@@ -154,37 +160,60 @@ let acertos = 0;
 let questaoUsada = [];
 let opcoesAtuais = [];
 
+//Função para mostrar o texto e as opções
 function mostrarTextos() {
   const pergunta = document.querySelector("#pergunta");
 
+  //Gera o índice da pergunta aleatória
   let perguntaAleatoria = Math.floor(Math.random() * enunciados.length);
 
+  //Verifica se o índice gerado já está na lista de questões usadas
+  //Se estiver, gera outro índice
   while (questaoUsada.includes(perguntaAleatoria)) {
     perguntaAleatoria = Math.floor(Math.random() * enunciados.length);
   }
+
+  //Adiciona o índice da pergunta na lista de questões usadas
   questaoUsada.push(perguntaAleatoria);
 
+  //Pergunta atual recebe o valor do índice
   perguntaAtual = perguntaAleatoria;
 
+  //Coloca a pergunta na tela
   pergunta.innerHTML = enunciados[perguntaAleatoria].pergunta;
 
+  //Cria a lista para as respostas usadas e limpa a lista de opções
   let respostaUsada = [];
   opcoesAtuais = [];
+
+  //Repete a ação de sortear um índice para cada botão
   for (let i = 0; i < enunciados[perguntaAtual].opcoes.length; i++) {
     let questaoAleatoria = Math.floor(
       Math.random() * enunciados[perguntaAtual].opcoes.length,
     );
 
+    //Verifica se o índice gerado já está na lista de respostas usadas
+    //Se estiver, gera outro índice
     while (respostaUsada.includes(questaoAleatoria)) {
       questaoAleatoria = Math.floor(
         Math.random() * enunciados[perguntaAtual].opcoes.length,
       );
     }
+
+    //Adiciona o índice na lista de respostas usadas
     respostaUsada.push(questaoAleatoria);
+
+    //Adiciona na lista de opções a opção sorteada
     opcoesAtuais.push(enunciados[perguntaAtual].opcoes[questaoAleatoria]);
+
+    //Exibe os textos nos botões
     botoes[i].innerHTML = opcoesAtuais[i];
   }
 }
+
+//
+// PONTUAÇÃO E CONTADOR
+//
 
 const pontosFinal = document.querySelector("#pontuacao");
 const textoFinal = document.querySelector("#textoOfensivo");
@@ -192,14 +221,23 @@ const textoFinal = document.querySelector("#textoOfensivo");
 const numQuestao = document.querySelector("#numeroQuestao");
 let contador = 1;
 
-const total = document.querySelector("#total")
+const total = document.querySelector("#total");
+
+let respostas = [];
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].addEventListener("click", () => {
-    if (opcoesAtuais[i] == enunciados[perguntaAtual].resposta) {
+    if (opcoesAtuais[i] === enunciados[perguntaAtual].respostaCerta) {
       acertos++;
     }
+
     quantidadePergunta++;
+
+    respostas.push({
+      numero: contador,
+      questao: perguntaAtual,
+      escolha: opcoesAtuais[i],
+    });
 
     if (quantidadePergunta >= enunciados.length) {
       quiz.classList.add("desativado");
@@ -208,7 +246,7 @@ for (let i = 0; i < botoes.length; i++) {
       mostrarTextos();
     }
 
-    contador += 1;
+    contador++;
     numQuestao.innerHTML = `${contador}`;
 
     pontosFinal.innerHTML = `${acertos}`;
@@ -218,20 +256,79 @@ for (let i = 0; i < botoes.length; i++) {
       textoFinal.innerHTML =
         "Vamos deixar de ser sem cultura e se informar um pouco mais!";
     } else if (acertos <= 9) {
-      textoFinal.innerHTML = "Tá bom até eu esperava muito menos de você";
+      textoFinal.innerHTML = "Tá bom, até eu esperava muito menos de você.";
     } else if (acertos <= 14) {
       textoFinal.innerHTML =
         "Eu não acredito que você sabe tudo isso de questão!";
     } else {
-      textoFinal.innerHTML = "Meus parabens, até merece um prêmio";
+      textoFinal.innerHTML = "Meus parabéns, até merece um prêmio.";
     }
   });
 }
+
+//
+// TELA FIM
+//
+
 const Bvoltar = document.querySelector("#Bvoltar");
 
 Bvoltar.addEventListener("click", () => {
   fim.classList.add("desativado");
   menu.classList.remove("desativado");
   footer.classList.remove("desativado");
+
   contador = 1;
+});
+
+//
+// TELA DE RESPOSTAS
+//
+
+const Bresposta = document.getElementById("Bresp");
+const BVoltarResp = document.getElementById("BRespvoltar");
+const TelaResposta = document.querySelector("#resposta");
+
+BVoltarResp.addEventListener("click", () => {
+  TelaResposta.classList.add("desativado");
+  menu.classList.remove("desativado");
+  footer.classList.remove("desativado");
+
+  contador = 1;
+});
+
+Bresposta.addEventListener("click", () => {
+  fim.classList.add("desativado");
+  TelaResposta.classList.remove("desativado");
+
+  const containerQuestao = document.querySelector(".cont-resp");
+  const boxquestao = document.querySelector(".quadro-resp");
+  const copias = containerQuestao.querySelectorAll(".copias");
+
+  for (let i = 0; i < copias.length; i++) {
+    copias[i].remove();
+  }
+
+  for (let i = 0; i < respostas.length; i++) {
+    const copiaBoxQuestao = boxquestao.cloneNode(true);
+
+    copiaBoxQuestao.classList.add("copias");
+    copiaBoxQuestao.classList.remove("desativado");
+
+    const ResNumero = copiaBoxQuestao.querySelector(".resp-numero");
+    const ResQuestao = copiaBoxQuestao.querySelector(".resp-questao");
+    const respCerta = copiaBoxQuestao.querySelector(".resp-certa");
+    const respEscolhida = copiaBoxQuestao.querySelector(".resp-escolhida");
+
+    ResNumero.innerHTML = `${respostas[i].numero}`;
+    ResQuestao.innerHTML = `${enunciados[respostas[i].questao].pergunta}`;
+
+    respEscolhida.innerHTML = respostas[i].escolha
+
+    respCerta.innerHTML = `${enunciados[respostas[i].questao].respostaCerta}`;
+    respCerta.style.color = "green";
+
+    containerQuestao.appendChild(copiaBoxQuestao);
+  }
+
+  boxquestao.classList.add("desativado");
 });
